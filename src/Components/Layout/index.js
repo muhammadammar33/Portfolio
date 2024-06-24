912548a446d8c58812075c6c647311be2d0c6de4
Muhammad Ammar Cheema <muhammadammar7747@gmail.com>
2024-06-24 21:00:14 +0500

    Layout Component

---
+++ src/Components/Layout/index.js
@@ -0,0 +1,19 @@
+import './index.scss';
+
+const Layout = () => {
+    return (
+        <div className="layout">
+            <div className="layout__header">
+                <h1>Header</h1>
+            </div>
+            <div className="layout__body">
+                <h2>Body</h2>
+            </div>
+            <div className="layout__footer">
+                <h3>Footer</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Layout;
